5132daac6a9c4b80cd46cc97d57318b416e2fa5e
yogeash-nehra <yogi.nehra14@gmail.com>
2024-10-14 19:03:54 +1300

    Initial, First (Class) and Second (Function) Components added.

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,13 @@
+import {Component} from "react";
+class FirstComponent extends Component{
+    render() {
+        return(
+            <div>
+                <h1>My first Component</h1>
+
+            </div>
+        )
+    }
+}
+
+export default FirstComponent;
---
+++ src/components/SecondComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SecondComponent(props) {
+    return (
+        <div>
+            <h1>The Second Component, First Function</h1>
+
+        </div>
+    )
+        ;
+}
+
+export default SecondComponent;
